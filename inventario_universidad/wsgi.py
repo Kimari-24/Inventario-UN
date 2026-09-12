ac32882184cbb63b641312db69f48c065db99bbd
Kimari-24 <juanmartinez2402003@gmail.com>
2026-09-12 11:45:06 -0500

    commit

--- inventario_universidad/wsgi.py
+++ inventario_universidad/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for inventario_universidad project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
 
 import os
 
